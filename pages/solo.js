e31375f889964c25e9804bbe1042034be3adcf5d
Sarah Katz <soshasarah@gmail.com>
2018-01-03 21:12:22 -0500

    minor addition to project

--- pages/solo.js
+++ pages/solo.js
@@ -13,7 +13,7 @@ export default () => {
             company="Fullstack Academy" 
             description="This app, built for a five-day hackathon, allows users to input a race, date and location, and when they log in after the race, they will be shown some pictures taken nearby that can serve as inspiration for their post-race medal pictures. Users can also connect a Strava account and pull their latest workout data to receive photo inspiration. It features a front end and store state built with React-Redux, a backend built with Node.js and Express, and a PostgreSQL database to store user and race information."
             live="http://racemedalpics.herokuapp.com"
-            tech="React, Redux, React-Redux, Flickr API, Nominatim API, Strava API"/>
+            tech="React, Redux, React-Redux, Flickr API, Nominatim API, Strava API, Passport, OAuth"/>
         </div>
       </div>
       <style jsx>{`
